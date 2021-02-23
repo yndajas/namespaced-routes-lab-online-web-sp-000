f6529f257afa2fbc3db573684679ea58a92395d6
Ynda Jas <yndajas@gmail.com>
2021-02-23 19:09:02 +0000

    commit tree a608d92db42bc66454b1848a009be83d3643a28b

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,11 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    if Preference.first.allow_create_songs == false
+      redirect_to songs_path
+    else
+      @song = Song.new
+    end
   end
 
   def create
